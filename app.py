7dbc8dcc99cb405c5adf1e3da46f47b56b1aa21f
James English <jamestenglish@gmail.com>
2016-10-13 21:09:55 -0400

    demo

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from demo import ANSWER
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Flask Dockerized'
+
+@app.route('/demo'):
+    return ANSWER
+
+@app.route('/health')
+def health():
+    return 'healthy'
+
+if __name__ == '__main__':
+    app.run(debug=False,host='0.0.0.0',port=8080)
+
